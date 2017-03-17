7b1992ed4a5888126cb6f9e6319f14f156032df0
CherutoMercy <m.thirty100@gmail.com>
2017-03-18 00:05:51 +0300

    add saolon routes

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,23 @@ var router         = express.Router();
 var homeRoutes     = require('./home');
 var landRoutes     = require('./land');
 var userRoutes     = require('./user');
+var salonRoutes     = require('./salon');
+
 router.get('/',        homeRoutes.index);
 router.get('/landing', landRoutes.index);
-router.get('/profile', userRoutes.index);
-router.get('/signup', userRoutes.signup);
 
+/*
+Salon Routes
+*/
+router.get('/salonindex', salonRoutes.index);
+router.get('/salonnew', salonRoutes.new);
+router.get('/salonedit', salonRoutes.edit);
+router.get('/salonprofile', salonRoutes.profile);
+
+/*
+User Routes
+*/
+router.get('/userindex', userRoutes.index);
+router.get('/signup', userRoutes.signup);
+router.get('/userprofile', userRoutes.profile);
 module.exports=router;
