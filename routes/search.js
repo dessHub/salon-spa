faf239b4bdbd55d2393c7ed1fecd15aec65f3b68
CherutoMercy <m.thirty100@gmail.com>
2017-03-28 23:07:43 +0300

    add search routes and salon routes

--- routes/search.js
+++ routes/search.js
@@ -1,7 +1,27 @@
 module.exports = {
   index : function(req, res){
-      res.render('search/find',{
+      res.render('search/pages/index',{
       });
   },
+  
+  single : function(req, res){
+      res.render('search/pages/single',{
+      });
+  },
+
+  service : function(req, res){
+      res.render('search/pages/services',{
+      });
+  },
+
+  hair : function(req, res){
+      res.render('search/pages/trends',{
+      });
+  },
+
+  offer : function(req, res){
+      res.render('search/pages/offers',{
+      });
+  }
 
 };
