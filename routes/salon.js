b5192b23f09cc621594e6efcbc1bcfda7ba64cf7
CherutoMercy <m.thirty100@gmail.com>
2017-04-17 06:48:20 +0300

    update service

--- routes/salon.js
+++ routes/salon.js
@@ -1,13 +1,27 @@
 const Service     = require('../models/service');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+//const upload = multer({dest : '/uploads'});
 
+const storage =multer.diskStorage({
+  destination:(req, file, callback)=>{
+    callback(null, './uploads');
+  },
+  filename: (req, file, callback)=>{
+    callback(null, file.fieldname + '-' +
+      Date.now());
+  }
+});
 
+const upload =multer({ storage : storage}).single('userPhoto');
 
 module.exports = {
   index : (req, res)=> {
    res.render('dashboard/salons/index');
   },
 
- service: (req,res)=> {
+  services: (req,res)=> {
     Service.find({},(err,service)=>{
       if(err) res.send(err);
       res.render('dashboard/salons/salonservices', {
@@ -15,22 +29,127 @@ module.exports = {
       });
     });
   },
-  
-  newservice: (req, res)=> {
-    const newservice = new Service();
-    newservice.title = req.body.title;
-    newservice.description = req.body.description;
-    //newservice.img.data = fs.readFileSync(req.files.userPhoto.path)
-    //newservice.img.contentType - '';
 
-    newservice.save((err,service)=>{
+  addservice: (req,res)=> {
+    Service.find({},(err,service)=>{
+      if(err) res.send(err);
+      res.render('dashboard/salons/service');
+    });
+  },
+
+  servicephoto: (req,res)=>{
+    upload(req,res, (err)=>{
+      if(err){
+        return res.end("Error uploading file");
+      }
+      console.log(req.file);
+      console.log(req.file.path);
+    //const image = req.userPhoto;
+    //console.log(image);
+    const tmp_path = req.file.path;
+
+
+                 /** The original name of the uploaded file
+           stored in the variable "originalname". **/
+    const target_path = 'uploads/' + req.file.originalname;
+       /** A better way to copy the uploaded file. **/
+    const src = fs.createReadStream(tmp_path);
+    const dest = fs.createWriteStream(target_path);
+    src.pipe(dest);
+    fs.unlink(tmp_path); //deleting the tmp_path
+
+    const service = new Service();
+    service.title = req.body.title;
+    service.description = req.body.description;
+    service.img = target_path;
+
+    service.save((err,service)=>{
+      if(err)res.send(err);
+      console.log(service)
+      res.redirect('/service');
+    });
+    });
+  },
+  
+/*router.post('/api/photo',function(req,res){
+ var newItem = new Item();
+ newItem.img.data = fs.readFileSync(req.files.userPhoto.path)
+ newItem.img.contentType = 'image/png';
+ newItem.save();
+});*/
+
+  postservice: (req, res, next)=> {
+    
+    const title = req.body.title;
+    console.log(title);
+    const image = req.userPhoto;
+    console.log(image);
+    const tmp_path = req.file.path;
+
+
+           /** The original name of the uploaded file
+           stored in the variable "originalname". **/
+    const target_path = 'uploads/' + req.file.originalname;
+       /** A better way to copy the uploaded file. **/
+    const src = fs.createReadStream(tmp_path);
+    const dest = fs.createWriteStream(target_path);
+    src.pipe(dest);
+    fs.unlink(tmp_path); //deleting the tmp_path
+
+    const service = new Service();
+    service.title = req.body.title;
+    service.description = req.body.description;
+    service.img = tmp_path;
+
+    service.save((err,service)=>{
       if(err)res.send(err);
       console.log(service)
       res.redirect('/service');
     });
       
   },
+  
+  deleteservice : (req, res)=>{
+    Service.findOne({_id:req.params.id}, (err, service)=>{
+      if(err) res.send(err);
 
+      if(service){
+        service.remove({}, (err, fashions)=> {
+          if (err) res.send(err);
+            console.log('service delete');
+             res.redirect('/service');
+          });
+        }else{
+          res.send("service not deleted");
+        }
+    });
+  },
+
+  editservice: (req, res)=>{
+    Service.findOne({ _id : req.params.id },(err, service)=>{
+      if(err) return err;
+      res.render('dashboard/salons/editservice', {
+          title      : "update",
+          service    : service
+      });
+    });  
+  },
+
+  updateservice: (req, res, next)=>{
+    console.log(req.params.id);
+   // Service.findOne({ _id : req.body.serviceid},(err, service)=>{
+       // if(err) return next(err);
+       const title = req.body.title;
+       const description = req.body.description;
+       const service = new Service();
+        service._id =  req.params.id;
+        service.update({title:title, description:description}, (err, service)=>{
+          if(err) return next(err);
+          console.log(service)
+          res.redirect('/service');
+        });
+      //});
+     },
 
   product: (req, res)=> {
       res.render('dashboard/salons/salonproducts');
