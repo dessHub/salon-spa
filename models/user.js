2cc9ac854ba13308c19776634a9866b6f11e84fc
CherutoMercy <m.thirty100@gmail.com>
2017-04-17 06:36:46 +0300

    add session routes

---
+++ models/user.js
@@ -0,0 +1,45 @@
+var mongoose   = require('mongoose');
+var bcrypt     = require('bcryptjs');
+var crypto     = require('crypto');
+var Schema     = mongoose.Schema;
+var Role       = require('./role.js');
+
+var UserSchema = Schema({
+    username: { type: String, index: true },
+    password: { type: String },
+    fname: { type: String },
+    lname: { type: String },
+    email:    { type: String },
+    companyname: {type: String},
+    country:     {type:String},
+    address:     {type:String},
+    town:        {type:String},
+    county:      {type:String},
+    postalcode:  {type: Number},
+    role:     [{type: Schema.Types.ObjectId, ref:Role }]
+});
+
+var User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.createUser = function(newUser,callback){
+  bcrypt.genSalt(10,function(err,salt){
+    if(err) throw err;
+    bcrypt.hash(newUser.password,salt,function(err,hash){
+      newUser.password = hash;
+      newUser.save(callback);
+    });
+  });
+};
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+  bcrypt.compare(candidatePassword, hash, callback);
+};
+
+module.exports.getUserByUsername = function(username, callback){
+  User.findOne(username,callback);
+};
+
+module.exports.getUserById = function(id,callback){
+  User.findById(id,callback);
+};
+
