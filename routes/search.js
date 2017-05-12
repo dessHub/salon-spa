f913f8fb6f4730091ea940db69504341b851b07d
CherutoMercy <m.thirty100@gmail.com>
2017-05-12 13:00:25 +0300

    find services,news and products from db

--- routes/search.js
+++ routes/search.js
@@ -1,45 +1,88 @@
 const Salon     = require('../models/salon');
 const Adminnews =require('../models/adminnews');
+const Service     = require('../models/service');
+const Style     = require('../models/style');
+const Product     = require('../models/product');
 
 module.exports = {
   index: (req, res)=> {
+    Adminnews.find({},(err,adminnews)=>{
+      if(err) res.send(err);
+   if (adminnews){
     Salon.find({},(err,salon)=>{
       if(err) res.send(err);
       res.render('search/pages/index', {
+         adminnews:adminnews,
          salon:salon
       });
     });
+      }
+});
   },
 
  item : function(req, res){
-    Adminnews.findOne({},(err,adminnews)=>{
-   if(err) res.send(err);
+    Adminnews.find({},(err,adminnews)=>{
+      if(err) res.send(err);
    if (adminnews){
     Salon.findOne({_id:req.params.id}, function(err, salon){
       if(err) res.send(err);
             res.render('search/pages/single',{
-                salon:salon,
-                adminnews:adminnews
+                adminnews:adminnews,
+                salon:salon
             });
         });
     }
 });
 }, 
-  
 
+/*  getbytitle: ((req, res) => {
+        const title = req.params.title;
+        if (title) {
+            Event.find({ title: req.params.title })
+*/
   service : (req, res)=> {
-      res.render('search/pages/services',{
+    Adminnews.find({},(err,adminnews)=>{
+      if(err) res.send(err);
+   if (adminnews){
+    Service.find({},(err,service)=>{
+      if(err) res.send(err);
+      res.render('search/pages/services', {
+         adminnews:adminnews,
+         service:service
       });
+    });
+      }
+    });
   },
 
   hair : (req, res)=> {
-      res.render('search/pages/trends',{
+    Adminnews.find({},(err,adminnews)=>{
+      if(err) res.send(err);
+   if (adminnews){
+    Style.find({},(err,style)=>{
+      if(err) res.send(err);
+      res.render('search/pages/trends', {
+         adminnews:adminnews,
+         style:style
       });
+    });
+      }
+    });
   },
 
   offer : (req, res)=> {
-      res.render('search/pages/offers',{
+    Adminnews.find({},(err,adminnews)=>{
+      if(err) res.send(err);
+   if (adminnews){
+    Product.find({},(err,product)=>{
+      if(err) res.send(err);
+      res.render('search/pages/offers', {
+         adminnews:adminnews,
+         product:product
       });
+    });
+      }
+    });
   },
 
   appointment : (req, res)=> {
